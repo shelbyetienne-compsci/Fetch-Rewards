8df6b27fe56fd6196ac27a0a3988643d1d1770b4
Shelby Etienne <67346746+shelbyetienne-compsci@users.noreply.github.com>
2024-09-11 12:42:10 -0400

    clean up

--- app/src/main/java/com/example/fetchrewards/MainActivity.kt
+++ app/src/main/java/com/example/fetchrewards/MainActivity.kt
@@ -14,9 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fetchrewards.ui.theme.FetchRewardsTheme
 
