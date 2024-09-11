a9de8666aea7fc6b657aab188cda134dfe80bc62
Shelby Etienne <67346746+shelbyetienne-compsci@users.noreply.github.com>
2024-09-11 12:40:00 -0400

    make it look nice

--- app/src/main/java/com/example/fetchrewards/HiringListComponent.kt
+++ app/src/main/java/com/example/fetchrewards/HiringListComponent.kt
@@ -1,26 +1,42 @@
 package com.example.fetchrewards
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun HiringList(viewModel: HiringViewModel=viewModel()){
+fun HiringList(viewModel: HiringViewModel = viewModel()) {
     val items by viewModel.hiringData
 
     val groups = items.groupBy { it.listId }.toSortedMap()
 
-    LazyColumn {
-        groups.forEach{ group ->
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        groups.forEach { group ->
             stickyHeader {
-                Column {
-                    Text(text = group.key.toString())
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                ) {
+                    Text(
+                        text = group.key.toString(),
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
 
@@ -34,5 +50,5 @@ fun HiringList(viewModel: HiringViewModel=viewModel()){
 
 @Composable
 fun HiringListItem(hiringItem: HiringItem) {
-    Text(text = hiringItem.name!!)
+    Text(text = hiringItem.name!!, fontSize = 18.sp, modifier = Modifier.padding(8.dp))
 }
